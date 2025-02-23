4257ecf996e5d9f33006c85293deeadb3da0719a
duyquan3112 <quan.nguyenduy@mfast.vn>
2025-02-23 21:10:19 +0700

    init

---
+++ src/main/java/com/newbie/identityService/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.newbie.identityService.repository;
+
+import com.newbie.identityService.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
